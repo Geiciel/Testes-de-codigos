c8f76a5b465c78f3066a6177fe380a5fc58923d5
Geiciel <geicielmarques@gmail.com>
2024-10-31 19:51:55 -0300

    Teste coação 0.2

--- Coração python - teste 5/Teste.py
+++ Coração python - teste 5/Teste.py
@@ -10,12 +10,20 @@ def heartb(k):
     math.cos(3*k)-\
     math.cos(4*k)
 
-speed(0)
+speed(100)
 bgcolor("black")
 
-for i in range(9000):
+for i in range(6000):
     goto(hearta(i)*20,heartb(i)*20)
     for j in range(1):
         color("red")
     goto(0,0)
+
+    def reveal_name():
+        goto(0, -45)
+        color("black")
+        style = ('Arial', 50, 'bold')
+        write("Eu te amo Monyse", font=style, align="center")
+    reveal_name()
+
 done()
